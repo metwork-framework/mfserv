6a54f4b1c970d1d971b305ef0ebf5f916c86b6f6
Fabien MARTY <fabien.marty@gmail.com>
2019-01-03 10:24:04 +0100

    feat: provide a way for plugins to launch their own daemons
    Close #39

---
+++ integration_tests/0003_extra_daemons/_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+plugins.uninstall foobar >/dev/null 2>&1
+
+set -x
+set -e
+
+mfserv.start
+
+rm -f "${MODULE_RUNTIME_HOME}/log/extra_daemon_foo_plugin_foobar.log"
+
+cd foobar
+make develop
+
+sleep 5
+timeout 10s wget -O toto "http://127.0.0.1:${MFSERV_NGINX_PORT}/foobar"
+N=$(cat toto |grep -c Hello)
+if test "${N}" -ne 1; then
+    echo "bad output"
+    cat toto
+fi
+circus_status_watcher.sh extra_daemon_foo_for_plugin_foobar
+N=$(cat "${MODULE_RUNTIME_HOME}/log/extra_daemon_foo_plugin_foobar.log" |grep -c foobar)
+if test "${N}" -ne 1; then
+    echo "bad log"
+    cat "${MODULE_RUNTIME_HOME}/log/extra_daemon_foo_plugin_foobar.log"
+fi
+sleep 10
+
+plugins.uninstall foobar >/dev/null 2>&1
+mfserv.stop
