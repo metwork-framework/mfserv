866b94c5125f9124afa9f1fab8d43a291430c929
thebaptiste <jbaptiste31@free.fr>
2019-01-25 10:52:52 +0000

    feat: execute integration tests directly from mfserv module and lauch them on a pull request on the module

--- integration_tests/0002_python3_plugin/test.sh
+++ integration_tests/0002_python3_plugin/test.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+plugins.uninstall foobar >/dev/null 2>&1
+rm -Rf foobar*
+
 set -x
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mfserv.start
+bootstrap_plugin.py create --no-input foobar
 
-su --command="${DIR}/_test.sh" - mfserv
+cd foobar
+echo django >python3_virtualenv_sources/requirements-to-freeze.txt
+make release
+plugins.install "$(ls *.plugin)"
+plugins.uninstall foobar
+mfserv.stop
+rm -Rf foobar*
---
+++ run_integration_tests.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# automatically generated from https://github.com/metwork-framework/resources/blob/master/cookiecutter/_%7B%7Bcookiecutter.repo%7D%7D/mfxxx_run_integration_tests.sh template
+
+cd integration_tests 2>/dev/null
+
+if test $? != 0; then
+    echo "Directory integration_tests is missing"
+    exit 0
+fi
+
+list_rep=`ls`
+if test -z "$list_rep"; then
+    echo "There are no integration tests"
+    exit 0
+fi
+
+ret=0
+for rep in $list_rep; do
+    if [ -d $rep ]; then
+        cd $rep
+        if test -s .layerapi2_dependencies; then
+             LAYERS_TO_LOAD=`cat .layerapi2_dependencies |xargs |sed 's/ /,/g'` 
+             WRAPPER=0
+        else
+             WRAPPER=1
+        fi
+        for test in test*; do
+            echo "Test" $test "in" $rep
+            if test $WRAPPER -eq 0; then
+                layer_wrapper --layers=$LAYERS_TO_LOAD -- ./$test
+            else
+                ./$test
+            fi
+            if test $? == 0; then
+                echo "Test $test OK"
+            else
+                echo "Test $test KO"
+                ret=1
+            fi
+        done
+        cd ..
+    fi
+done
+
+exit $ret
