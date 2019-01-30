5f29c69596bdbf47f0692ed02ed57bff9f6537cf
Fabien MARTY <fabien.marty@gmail.com>
2019-01-30 17:50:54 +0100

    test: dont start/stop mfserv
    (cherry picked from commit 0cb2d837019cdffafc17882a2281b825aac388ec)

--- integration_tests/0002_python3_plugin/test.sh
+++ integration_tests/0002_python3_plugin/test.sh
@@ -6,7 +6,6 @@ rm -Rf foobar*
 set -x
 set -e
 
-mfserv.start
 bootstrap_plugin.py create --no-input foobar
 
 cd foobar
@@ -14,5 +13,4 @@ echo django >python3_virtualenv_sources/requirements-to-freeze.txt
 make release
 plugins.install "$(ls *.plugin)"
 plugins.uninstall foobar
-mfserv.stop
 rm -Rf foobar*
