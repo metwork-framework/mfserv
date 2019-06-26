c78e123a8d19c6e0eddd4d912976289bb56a48e9
BenjaminSou <benjamin.soulan@depinfonancy.net>
2019-06-26 09:25:56 +0200

    doc: add doc in flask template
    Also:
    Minor modification to show "Hello World !" instead of "Hello unknown !"
    Modification in integration test to take it in count.

--- adm/templates/plugins/flask/{{cookiecutter.name}}/main/wsgi.py
+++ adm/templates/plugins/flask/{{cookiecutter.name}}/main/wsgi.py
@@ -1,28 +1,34 @@
+"""
+Main code of the template exemple to use Flask.
+"""
+
 from flask import Flask, render_template
 from jinja2 import Template
+
 app = Flask(__name__)
 
-
-template = Template('{% raw %}{{ name }}{% endraw %} je te salue')
-
-
+# Simple use of appi with jinja implements {%raw%}{{}}{%endraw%}
 @app.route("/{{cookiecutter.name}}/")
+def hello_world():
+    return "Hello World !"
 @app.route("/{{cookiecutter.name}}/<name>")
 def hello(name="unknown"):
     return render_template("template_urlparse/index.html", name=name)
 
-
+# rendering html (and other files types) template.
 @app.route("/{{cookiecutter.name}}/webpage")
 def webpage():
     return render_template("template_webpage/index.html")
 
+template = Template('{% raw %}{{ name }}{% endraw %} je te salue')
 
+# Insert variables with name and send it to the rendering.
 @app.route("/{{cookiecutter.name}}/jinja2/<name>")
 def jinja(name):
     return (template.render(name=name) +
             render_template("template_jinja_examples/index.html", name=name))
 
-
+# Simple error handler
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
