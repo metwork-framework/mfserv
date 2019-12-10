9708d53456d284c0f428b7d0d72943a1af13718a
Fabien MARTY <fabien.marty@gmail.com>
2019-12-10 17:23:52 +0100

    build: refactor nodejs backend to use signal_wrapper.py

--- layers/layer1_nodejs/0500_metwork_tools/metwork-tools/index.js
+++ layers/layer1_nodejs/0500_metwork_tools/metwork-tools/index.js
@@ -1,7 +1,5 @@
 const process = require('process')
 const fs = require('fs')
-const http = require('http')
-const base64url = require('base64url')
 const fsext = require('fs-ext')
 const util = require('util')
 
@@ -19,48 +17,24 @@ const PID = process.pid
 const PLUGIN = process.env.MFSERV_CURRENT_PLUGIN_NAME || "unknown"
 
 function before_start(unix_socket_path) {
-    if (fs.existsSync(unix_socket_path) === true) {
-        try {
-            fs.unlinkSync(unix_socket_path)
-        } catch(e) {
-            process.stderr.write("can't unlink: " + unix_socket_path + " => exiting\n")
-            process.exit(1)
-        }
-    }
+    // DEPRECATED
+    process.stdout.write("metwork_tooks.before_start() is deprecated => remove this call from your code")
 }
 
 function after_start(unix_socket_path) {
-    var req = http.get("http://127.0.0.1:" + process.env.MFSERV_NGINX_PORT + "/__socket_up/" + base64url(unix_socket_path), function(res) {
-        res.resume()
-        process.stdout.write("ok: process #" + process.pid + " is listening on " + unix_socket_path + "\n")
-    })
-    req.on('error', function(e) {
-        // maybe we are in starting mode and nginx is not started yet
-        process.stdout.write("ok: process #" + process.pid + " is listening on " + unix_socket_path + "\n")
-    })
+    // DEPRECATED
+    process.stdout.write("ok: process #" + process.pid + " is listening on " + unix_socket_path + "\n")
 }
 
 function before_stop(unix_socket_path) {
-    process.stdout.write("SIGTERM catched => calling close\n")
-    var req = http.get("http://127.0.0.1:" + process.env.MFSERV_NGINX_PORT + "/__socket_down/" + base64url(unix_socket_path) + "?wait=1", function(res) {
-        res.resume()
-        server.close()
-    })
-    req.on('error', function(e) {
-        // nginx is not available ?, we are maybe in stopping mode
-        server.close()
-    })
+    // DEPRECATED
+    process.stdout.write("metwork_tooks.before_stop() is deprecated => remove this call from your code")
+    server.close()
 }
 
 function after_stop(unix_socket_path) {
+    // DEPRECATED
     process.stdout.write("exiting !\n")
-    if (fs.existsSync(unix_socket_path) === true) {
-        try {
-            fs.unlinkSync(unix_socket_path)
-        } catch(e) {
-            process.stderr.write("can't unlink: " + unix_socket_path + " => exiting\n")
-        }
-    }
 }
 
 function _console_log(level, data, ...args) {
