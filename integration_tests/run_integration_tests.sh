6b2629af8676e44146d6dd66a4d3006120d06f5b
metworkbot <marty.fabien@orange.fr>
2020-05-15 01:35:05 +0000

    chore: sync common files from resources repository

--- integration_tests/run_integration_tests.sh
+++ integration_tests/run_integration_tests.sh
@@ -10,59 +10,59 @@ fi
 for rep in $list_rep; do
     if [ -d $rep ]; then
         cd $rep
-        if test -s .layerapi2_dependencies; then
-             LAYERS_TO_LOAD=`cat .layerapi2_dependencies |xargs |sed 's/ /,/g'`
-             WRAPPER=0
-        else
-             WRAPPER=1
-        fi
-        for test in test*; do
-            echo "Test" $test "in" $rep
-            for F in $(ls ${MFMODULE_RUNTIME_HOME}/log/*.log ${MFMODULE_RUNTIME_HOME}/log/*.stdout ${MFMODULE_RUNTIME_HOME}/log/*.stderr 2>/dev/null); do
-                truncate -s 0 "${F}"
-            done
-            if test $WRAPPER -eq 0; then
-                run_test=1
-                missing_layers=""
-                if test -s .bypass_test_if_missing; then
-                    for layer in `cat .bypass_test_if_missing`; do
-                       if test `is_layer_installed ${layer}` -eq 0; then
-                           missing_layers="${missing_layers}"" ""${layer}"
-                           run_test=0
-                       fi
-                    done
+        run_test=1
+        missing_layers=""
+        if test -s .bypass_test_if_missing; then
+            for layer in `cat .bypass_test_if_missing`; do
+                if test `is_layer_installed ${layer}` -eq 0; then
+                    missing_layers="${missing_layers}"" ""${layer}"
+                    run_test=0
                 fi
-                if test ${run_test} = 1; then
+            done
+        fi
+        if test ${run_test} = 0; then
+            echo "Tests not run on ${rep} because of missing layer(s) ${missing_layers}"
+        else
+            if test -s .layerapi2_dependencies; then
+                 LAYERS_TO_LOAD=`cat .layerapi2_dependencies |xargs |sed 's/ /,/g'`
+                 WRAPPER=0
+            else
+                 WRAPPER=1
+            fi
+            for test in test*; do
+                echo "Test" $test "in" $rep
+                for F in $(ls ${MFMODULE_RUNTIME_HOME}/log/*.log ${MFMODULE_RUNTIME_HOME}/log/*.stdout ${MFMODULE_RUNTIME_HOME}/log/*.stderr 2>/dev/null); do
+                    truncate -s 0 "${F}"
+                done
+                if test $WRAPPER -eq 0; then
                     layer_wrapper --layers=$LAYERS_TO_LOAD -- ./$test
                 else
-                    echo $test "not run because of missing layer(s)" $missing_layers
+                    ./$test
                 fi
-            else
-                ./$test
-            fi
-            if test $? == 0; then
-                echo "Test $test ($rep) OK"
-            else
-                for F in $(ls ${MFMODULE_RUNTIME_HOME}/log/*.log ${MFMODULE_RUNTIME_HOME}/log/*.stdout ${MFMODULE_RUNTIME_HOME}/log/*.stderr 2>/dev/null); do
-                    if test -s "${F}"; then
-                        echo "===== 40 last lines of ${F} to debug ====="
-                        tail -40 "${F}"
-                        echo ""
-                        echo ""
-                    fi
-                done
-                for F in ${MFMODULE_RUNTIME_HOME}/tmp/config_auto/nginx.conf ${MFMODULE_RUNTIME_HOME}/tmp/config_auto/circus.ini; do
-                    if test -f "${F}"; then
-                        echo "===== ${F} content to debug ====="
-                        cat "${F}"
-                        echo ""
-                        echo ""
-                    fi
-                done
-                echo "Test $test ($rep) KO"
-                exit 1
-            fi
-        done
+                if test $? == 0; then
+                    echo "Test $test ($rep) OK"
+                else
+                    for F in $(ls ${MFMODULE_RUNTIME_HOME}/log/*.log ${MFMODULE_RUNTIME_HOME}/log/*.stdout ${MFMODULE_RUNTIME_HOME}/log/*.stderr 2>/dev/null); do
+                        if test -s "${F}"; then
+                            echo "===== 40 last lines of ${F} to debug ====="
+                            tail -40 "${F}"
+                            echo ""
+                            echo ""
+                        fi
+                    done
+                    for F in ${MFMODULE_RUNTIME_HOME}/tmp/config_auto/nginx.conf ${MFMODULE_RUNTIME_HOME}/tmp/config_auto/circus.ini; do
+                        if test -f "${F}"; then
+                            echo "===== ${F} content to debug ====="
+                            cat "${F}"
+                            echo ""
+                            echo ""
+                        fi
+                    done
+                    echo "Test $test ($rep) KO"
+                    exit 1
+                fi
+            done
+        fi
         cd ..
     fi
 done
