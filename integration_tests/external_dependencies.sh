bef062e3e5595d9e9d84e8b620861eff28d773fb
metworkbot <metworkbot@metwork-framework.org>
2020-12-15 14:21:57 +0000

    build: sync common files from github_organization_management repository

--- integration_tests/external_dependencies.sh
+++ integration_tests/external_dependencies.sh
@@ -11,4 +11,4 @@ if test "${1:-}" = "--help"; then
     exit 0
 fi
 
-( find . -type f -name "*.so*" -exec ldd {} 2>/dev/null \; ; find . -type f -wholename "*/bin/*" -exec ldd {} 2>/dev/null \;) |grep "=>" |awk -F '=>' '{print $2;}' |grep '/' |grep -v metwork |awk '{print $1;}' |sort |uniq
+( find . -type f -name "*.so*" -exec ldd {} 2>/dev/null \; ; find . -type f -wholename "./bin/*" -exec ldd {} 2>/dev/null \;) |grep "=>" |awk -F '=>' '{print $2;}' |grep '/' |grep -v metwork |awk '{print $1;}' |sort |uniq
--- integration_tests/external_dependencies_not_found.sh
+++ integration_tests/external_dependencies_not_found.sh
@@ -11,4 +11,4 @@ if test "${1:-}" = "--help"; then
     exit 0
 fi
 
-( find . -type f -name "*.so*" -exec ldd {} 2>/dev/null \; ; find . -type f -wholename "*/bin/*" -exec ldd {} 2>/dev/null \;) |grep "=>" | grep "not found" |awk -F '=>' '{print $1;}' |sort |uniq
+( find . -type f -name "*.so*" -exec ldd {} 2>/dev/null \; ; find . -type f -wholename "./bin/*" -exec ldd {} 2>/dev/null \;) |grep "=>" | grep "not found" |awk -F '=>' '{print $1;}' |sort |uniq
