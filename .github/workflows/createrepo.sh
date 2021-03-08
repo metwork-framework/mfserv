69666f9207903ba42c147265daf72fc0981d9c9a
metworkbot <metworkbot@metwork-framework.org>
2021-03-08 18:47:14 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/createrepo.sh
+++ .github/workflows/createrepo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Shell to be run on metwork-framework.org as metworkpub user (archived copy)
+#Shell to be run on metwork-framework.org as metworkpub user
 if test -d $1/.repodata; then
     for i in 1 2 3 4 5; do
         echo "Sleeping 30s, waiting lock on $1/.repodata directory"
         sleep 30
-        if test -d repodata2; then
+        if test -d $1/.repodata; then
             continue
         else
             break
