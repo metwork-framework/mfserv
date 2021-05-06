ec4e017959484bc825301715fe5fcec5d8769558
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-05-06 10:17:24 +0200

    build: update workflows (from common files)

--- .github/workflows/createrepo.sh
+++ .github/workflows/createrepo.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #Shell to be run on metwork-framework.org as metworkpub user
 if test -d $1/.repodata; then
-    for i in 1 2 3 4 5; do
-        echo "Sleeping 30s, waiting lock on $1/.repodata directory"
-        sleep 30
+    for i in 1 2 3 4 5 6 7 8 9 10; do
+        echo "Sleeping 1 minute, waiting lock on $1/.repodata directory"
+        sleep 60
         if test -d $1/.repodata; then
             continue
         else
             break
         fi
     done
+fi
+if test -d $1/.repodata; then
+    echo "Can't obtain lock on $1/.repodata directory"
+    exit 1
 fi
 createrepo --update $1
