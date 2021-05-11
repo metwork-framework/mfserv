30267d2f5e776bf2eb0d180d69d058cf85246b82
metworkbot <metworkbot@metwork-framework.org>
2021-05-11 15:19:47 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -98,5 +98,6 @@ echo "::set-output name=dep_dir::${DEP_DIR}"
 echo "::set-output name=buildimage::metwork/mfxxx-${OS_VERSION}-buildimage:${DEP_BRANCH}"
 echo "::set-output name=testimage::metwork/mfxxx-${OS_VERSION}-testimage:${DEP_BRANCH}"
 echo "::set-output name=buildlog_dir::/pub/metwork/${CI}/buildlogs/${B}/mfserv/${OS_VERSION}/${GITHUB_RUN_NUMBER}"
+
 echo "::set-output name=rpm_dir::/pub/metwork/${CI}/rpms/${B}/${OS_VERSION}"
 echo "::set-output name=doc_dir::/pub/metwork/${CI}/docs/${B}/mfserv"
