e47db0d7fdfcd35fbcad56116ba7d5764012ad88
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-05-18 16:40:37 +0200

    build: CI under github actions

---
+++ .github/workflows/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#set -eu
+set -x
+
+if test -d /buildcache; then export BUILDCACHE=/buildcache; fi
+
+#We keep the names DRONE_* with github_actions because they are used by guess_version.sh
+export DRONE_BRANCH=${BRANCH}
+export DRONE_TAG=${TAG}
+export DRONE=true
+
+    if test "${OS_VERSION}" = "centos6"; then export METWORK_BUILD_OS=generic; else export METWORK_BUILD_OS=${OS_VERSION}; fi
+
+
+
+
+
+cd /src
+
+
+
+
+
+mkdir -p "/opt/metwork-mfserv-${TARGET_DIR}"
+
+mkdir -p buildlogs
+export BUILDLOGS=buildlogs
+
+make >${BUILDLOGS}/make.log 2>&1 || ( tail -200 ${BUILDLOGS}/make.log ; exit 1 )
+
+OUTPUT=$(git status --short | grep -v buildlogs | grep -v buildcache)
+
+if test "${OUTPUT}" != ""; then
+    echo "ERROR non empty git status output ${OUTPUT}"
+    echo "git diff output"
+    git diff
+    exit 1
+fi
+
+ 
+
+if test -d docs; then make docs >${BUILDLOGS}/make_doc.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_doc.log ; exit 1 ); fi
+if test -d doc; then make doc >${BUILDLOGS}/make_doc.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_doc.log ; exit 1 ); fi
+rm -Rf html_doc
+if test -d /opt/metwork-mfserv-${TARGET_DIR}/html_doc; then cp -Rf /opt/metwork-mfserv-${TARGET_DIR}/html_doc . ; fi
+make test >${BUILDLOGS}/make_test.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_test.log ; exit 1 )
+make RELEASE_BUILD=${GITHUB_RUN_NUMBER} rpm >${BUILDLOGS}/make_rpm.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_rpm.log ; exit 1 )
+
+mkdir rpms
+mv /opt/metwork-mfserv-${TARGET_DIR}/*.rpm rpms
+
+ 
+
+echo "::set-output name=bypass::false"
---
+++ .github/workflows/integration_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+set -x
+
+cd /src
+
+
+echo -e "[metwork_${DEP_BRANCH}]" >/etc/yum.repos.d/metwork.repo
+echo -e "name=Metwork Continuous Integration Branch ${DEP_BRANCH}" >> /etc/yum.repos.d/metwork.repo
+echo -e "baseurl=http://metwork-framework.org/pub/metwork/continuous_integration/rpms/${DEP_BRANCH}/${OS_VERSION}/" >> /etc/yum.repos.d/metwork.repo
+echo -e "gpgcheck=0\n\enabled=1\n\metadata_expire=0\n" >>/etc/yum.repos.d/metwork.repo
+
+    yum -y localinstall ./rpms/metwork-mfserv*.rpm
+    yum -y install make
+
+    su --command="mfserv.init" - mfserv
+    su --command="mfserv.start" - mfserv
+    su --command="mfserv.status" - mfserv
+    if test -d "integration_tests"; then chown -R mfserv integration_tests; cd integration_tests; su --command="cd `pwd`; ./run_integration_tests.sh" - mfserv; cd ..; fi
+    su --command="mfserv.stop" - mfserv
+
