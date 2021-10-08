718fe8f6124d711bef106a5bacd709c0c35f2d46
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-10-08 10:52:32 +0200

    build: fix ci for repository_dispatch case (#524)
    * build: fix ci for repository_dispatch case
    
    * build: fix ci (repository_dispatch case)

--- .github/workflows/integration_tests.sh
+++ .github/workflows/integration_tests.sh
@@ -8,7 +8,7 @@ cd /src
 
 echo -e "[metwork_${DEP_BRANCH}]" >/etc/yum.repos.d/metwork.repo
 echo -e "name=Metwork Continuous Integration Branch ${DEP_BRANCH}" >> /etc/yum.repos.d/metwork.repo
-echo -e "baseurl=http://metwork-framework.org/pub/metwork/continuous_integration/rpms/${DEP_BRANCH}/${OS_VERSION}/" >> /etc/yum.repos.d/metwork.repo
+echo -e "baseurl=http://metwork-framework.org/pub/metwork/continuous_integration/rpms/${DEP_BRANCH}/centos6/" >> /etc/yum.repos.d/metwork.repo
 echo -e "gpgcheck=0\n\enabled=1\n\metadata_expire=0\n" >>/etc/yum.repos.d/metwork.repo
 
     yum -y localinstall ./rpms/metwork-mfserv*.rpm
