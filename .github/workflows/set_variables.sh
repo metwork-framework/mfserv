23bd0b08eafcba10c81f54e621b6498f88b1a4cc
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-10-08 10:29:51 +0200

    build: fix ci for repository_dispatch case (#522)

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -3,11 +3,6 @@
 #set -eu
 set -x
 
-if test "${OS_VERSION:-}" = ""; then
-    echo "ERROR: OS_VERSION env is empty"
-    exit 1
-fi
-
 TAG=
 DEP_BRANCH=
 TARGET_DIR=
@@ -95,8 +90,8 @@ echo "::set-output name=tag::${TAG}"
 echo "::set-output name=dep_branch::${DEP_BRANCH}"
 echo "::set-output name=target_dir::${TARGET_DIR}"
 echo "::set-output name=dep_dir::${DEP_DIR}"
-echo "::set-output name=buildimage::metwork/mfxxx-${OS_VERSION}-buildimage:${DEP_BRANCH}"
-echo "::set-output name=testimage::metwork/mfxxx-${OS_VERSION}-testimage:${DEP_BRANCH}"
-echo "::set-output name=buildlog_dir::/pub/metwork/${CI}/buildlogs/${B}/mfserv/${OS_VERSION}/${GITHUB_RUN_NUMBER}"
-echo "::set-output name=rpm_dir::/pub/metwork/${CI}/rpms/${B}/${OS_VERSION}"
+echo "::set-output name=buildimage::metwork/mfxxx-centos6-buildimage:${DEP_BRANCH}"
+echo "::set-output name=testimage::metwork/mfxxx-centos6-testimage:${DEP_BRANCH}"
+echo "::set-output name=buildlog_dir::/pub/metwork/${CI}/buildlogs/${B}/mfserv/centos6/${GITHUB_RUN_NUMBER}"
+echo "::set-output name=rpm_dir::/pub/metwork/${CI}/rpms/${B}/centos6"
 echo "::set-output name=doc_dir::/pub/metwork/${CI}/docs/${B}/mfserv"
