f344801d84fa14c1b41539930cde13ca7a66d313
Matthieu MARRAST <matthieu.marrast@gmail.com>
2021-12-02 17:57:51 +0100

    fix: rename default app name

--- adm/templates/plugins/python3_django3/hooks/pre_gen_project.py
+++ adm/templates/plugins/python3_django3/hooks/pre_gen_project.py
@@ -4,14 +4,14 @@
 """Tasks to be run before the project is generated.
 
 It checks that :
-- the plugin name is != hello (because it's the default app name)
+- the plugin name is != main (because it's the default app name)
 """
 
 
 import sys
 
 # the application name cannot be the same as its plugin name
-if 'hello' == '{{cookiecutter.name}}':
-    print('ERROR: you cant use hello as plugin name '
-          'because we create a default app named hello')
+if 'main' == '{{cookiecutter.name}}':
+    print('ERROR: you cant use main as plugin name '
+          'because we create a default app named main')
     sys.exit(1)
