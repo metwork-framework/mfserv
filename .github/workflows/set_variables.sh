dc699e76891e4765c5fc9a0c05a002cda0e7580c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-12-16 14:53:14 +0100

    build: fix ci in case of dispatch

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -31,6 +31,7 @@ esac
 if [ -z ${B} ]; then
   B=null
 fi
+SKIP_DISPATCH=false
 if [ "${GITHUB_EVENT_NAME}" != "repository_dispatch" ]; then
     case "${GITHUB_REF}" in
         refs/heads/experimental* | refs/heads/master | refs/heads/release_*)
@@ -45,7 +46,8 @@ if [ "${GITHUB_EVENT_NAME}" != "repository_dispatch" ]; then
             TAG=${GITHUB_REF#refs/tags/}
             DEP_BRANCH=${B}
             DEP_DIR=${B##release_}
-            TARGET_DIR=${B##release_};;
+            TARGET_DIR=${B##release_}
+            SKIP_DISPATCH=true;;
         refs/pull/*)
 case "${B}" in
                 integration | ci* | pci*)
@@ -93,5 +95,6 @@ echo "::set-output name=dep_dir::${DEP_DIR}"
 echo "::set-output name=buildimage::metwork/mfxxx-centos6-buildimage:${DEP_BRANCH}"
 echo "::set-output name=testimage::metwork/mfxxx-centos6-testimage:${DEP_BRANCH}"
 echo "::set-output name=buildlog_dir::/pub/metwork/${CI}/buildlogs/${B}/mfserv/centos6/${GITHUB_RUN_NUMBER}"
+echo "::set-output name=skip_dispatch::${SKIP_DISPATCH}"
 echo "::set-output name=rpm_dir::/pub/metwork/${CI}/rpms/${B}/centos6"
 echo "::set-output name=doc_dir::/pub/metwork/${CI}/docs/${B}/mfserv"
