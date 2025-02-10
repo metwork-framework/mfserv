b28d4f38448edcc6d4c0b5586592ac454be70343
metworkbot <metworkbot@metwork-framework.org>
2025-02-10 10:45:45 +0000

    build: sync common files from github_organization_management repository

--- integration_tests/0096_system_extra_dependencies/test.sh
+++ integration_tests/0096_system_extra_dependencies/test.sh
@@ -73,8 +73,8 @@ for layer in *; do
             continue
         fi
         echo "***** ${DEP} *****"
-        echo "=== revert ldd ==="
-        revert_ldd.sh "${DEP}"
+        echo "=== revert ldd not found ==="
+        revert_ldd_not_found.sh "${DEP}"
         echo
         echo
         RET=1
